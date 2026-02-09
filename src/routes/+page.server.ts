f6e090606ec3c329627b380bfbeaec25cd1132bd
anto4 <anto.hosek@gmail.com>
2026-02-09 18:14:57 +0100

    init 2

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,26 @@
+import { redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+import type { PageServerLoad } from './$types';
+import { auth } from '$lib/server/auth';
+
+export const load: PageServerLoad = async (event) => {
+  // Return user if authenticated, null otherwise
+  return { 
+    user: event.locals.user ?? null 
+  };
+
+	// to redirect instead:
+	// if (!event.locals.user) {
+	// 	return redirect(302, '/login');
+	// }
+	// return { user: event.locals.user };
+};
+
+export const actions: Actions = {
+  signOut: async (event) => {
+    await auth.api.signOut({
+      headers: event.request.headers
+    });
+    return redirect(302, '/login');
+  }
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang='ts'>
+  import { enhance } from '$app/forms';
+  import type { PageServerData } from './$types';
+  
+  let { data }: { data: PageServerData } = $props();
+</script>
+
+<svelte:head>
+  <title>Listoflinks</title>
+</svelte:head>
+  
+<h1>Listoflinks</h1>
+<p>Social media made of lists of links</p>
+
+{#if data.user}
+  <h1>Hi, {data.user.name}!</h1>
+  <p>Your user ID is {data.user.id}.</p>
+  <form method="post" action="?/signOut" use:enhance>
+    <button>Sign out</button>
+  </form>
+{:else}
+  <p>You are not connected.</p>
+  <a href="/login">Log in / register</a>
+{/if}
+
+
